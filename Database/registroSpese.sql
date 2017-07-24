78f9dc2aba4ea8c6b99a5805439c800060921283
Salvatore Cognetta <salvatore.cognetta@gmail.com>
2017-07-25 00:34:27 +0200

    Modificata query e aggiunte categorie

--- Database/registroSpese.sql
+++ Database/registroSpese.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `registrospese` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `registrospese`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
--- Host: localhost    Database: RegistroSpese
+-- Host: 127.0.0.1    Database: registrospese
 -- ------------------------------------------------------
--- Server version	5.5.5-10.1.25-MariaDB
+-- Server version	5.6.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +28,7 @@ CREATE TABLE `categoria` (
   `idcategoria` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idcategoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +37,7 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-INSERT INTO `categoria` VALUES (1,'Bollette'),(2,'Divertimento'),(3,'Bevande'),(4,'Sport');
+INSERT INTO `categoria` VALUES (1,'Bollette'),(2,'Divertimento'),(3,'Bevande'),(4,'Sport'),(5,'Abbigliamento'),(6,'Autostrada'),(7,'Altro'),(8,'Carburante'),(9,'Istruzione');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,7 +55,7 @@ CREATE TABLE `spesa` (
   `descrizione` varchar(45) DEFAULT NULL,
   `data` date DEFAULT NULL,
   PRIMARY KEY (`idspesa`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,13 +64,9 @@ CREATE TABLE `spesa` (
 
 LOCK TABLES `spesa` WRITE;
 /*!40000 ALTER TABLE `spesa` DISABLE KEYS */;
-INSERT INTO `spesa` VALUES (17,11.65,'Bollette','eee','2017-07-20'),(33,36.8,'Bollette','aaa','2017-07-24'),(34,22.75,'Bevande','qwerty','2017-07-11'),(35,234,'Sport','asd','2013-07-05'),(37,12,'Divertimento','tyu','2017-07-24'),(38,25,'Divertimento','aa','2017-07-06'),(40,11,'Divertimento','aaa','2018-07-07'),(41,22.36,'Bollette','asd','2017-07-06');
+INSERT INTO `spesa` VALUES (42,21.3,'Bollette','Enel','2017-07-19'),(43,55.5,'Sport','Piscina','2017-05-01'),(44,32,'Divertimento','Kart','2017-02-01'),(45,10,'Bevande','Acqua','2017-07-25'),(46,57,'Bollette','Gas','2017-07-25'),(49,29,'Bevande','Test','2016-07-01'),(50,150,'Divertimento','MotoGP','2017-09-05'),(51,80,'Bollette','Acqua','2017-07-01'),(52,55,'Abbigliamento','Scarpe','2017-07-25'),(53,25,'Autostrada','Firenze-Roma','2017-06-06'),(54,48,'Istruzione','Libro S.O.','2017-07-11'),(55,3,'Altro','Pellicola','2017-07-25');
 /*!40000 ALTER TABLE `spesa` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'RegistroSpese'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -79,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-25  1:54:03
+-- Dump completed on 2017-07-25  0:24:10
